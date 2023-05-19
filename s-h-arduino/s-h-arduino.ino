08e6226e9846441cad490c533aacf9fe2b4850d5
Abdullah Al Hadi Mahfuz <armahfuz47@gmail.com>
2023-05-19 21:34:51 +0600

    added esp32 - arduino communication

---
+++ s-h-arduino/s-h-arduino.ino
@@ -0,0 +1,66 @@
+#include <Keypad.h>  //door lock
+
+
+#define ROW_NUM 4     // four rows// door lock
+#define COLUMN_NUM 4  // four columns// door lock
+
+char keys[ROW_NUM][COLUMN_NUM] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};  // door lock
+
+byte pin_rows[ROW_NUM] = { 13, 12, 11, 10 };   // door lock
+byte pin_column[COLUMN_NUM] = { 9, 8, 7, 6 };  // door lock
+
+Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);  // door lock
+
+const String open_password = "1010A";   // // door lock
+const String close_password = "1010B";  // // door lock
+String input_password;                  // door lock
+
+void setup() {
+  Serial.begin(9600);
+  
+  input_password.reserve(32);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+
+    //ARDIO COMM
+  
+
+  //ARDIO COMM
+  //door lock START
+  //door lock START
+
+  char key = keypad.getKey();
+
+  if (key) {
+    
+
+    if (key == '*') {
+      input_password = "";  // clear input password
+    } else if (key == '#') {
+      if (open_password == input_password) {
+        Serial.print("o");
+      }
+      if (close_password == input_password) {
+        Serial.print("c");
+      }
+
+
+      else {
+        
+      }
+
+      input_password = "";  // clear input password
+    } else {
+      input_password += key;  // append new character to input password string
+    }
+  }
+}
+
+//Arduino Pin 13,12,11,10,9,8,7,6 for 4x4
--- s-h-esp32/s-h-esp32.ino
+++ s-h-esp32/s-h-esp32.ino
@@ -1,32 +1,19 @@
 #include <Servo.h>    //door lock
-#include <Keypad.h>   //door lock
 #include <Stepper.h>  // Window and Garage
 
+#define RXp2 16
+#define TXp2 17
+
 //door lock START
 //door lock START
 
+String a;
+String o = "o";
+String c = "c";
 
 
+Servo myservo;  // door lock
 
-Servo myservo;        // door lock
-#define ROW_NUM 4     // four rows// door lock
-#define COLUMN_NUM 4  // four columns// door lock
-
-char keys[ROW_NUM][COLUMN_NUM] = {
-  { '1', '2', '3', 'A' },
-  { '4', '5', '6', 'B' },
-  { '7', '8', '9', 'C' },
-  { '*', '0', '#', 'D' }
-};  // door lock
-
-byte pin_rows[ROW_NUM] = { 12, 14, 27, 26 };       // door lock
-byte pin_column[COLUMN_NUM] = { 25, 33, 32, 15 };  // door lock
-
-Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);  // door lock
-
-const String open_password = "1010A";   // // door lock
-const String close_password = "1010B";  // // door lock
-String input_password;                  // door lock
 
 
 int pos = 0;  // door lock
@@ -39,63 +26,59 @@ int feature = 2;
 
 //Garage START
 const int stepsPerRevolution = 2038;
-Stepper myStepper = Stepper(stepsPerRevolution, 21, 18, 19, 5);
+Stepper garageStepper = Stepper(stepsPerRevolution, 21, 18, 19, 5);
 //Garage END
+
+//Window START
+Stepper windowStepper = Stepper(stepsPerRevolution, 21, 18, 19, 5);
+
+//Window END
 
 
 void setup() {
 
+  Serial.begin(115200);
+  Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);
+
   //door lock START
 
   myservo.attach(13);  // attaches the servo on pin 13 to the servo object
-  Serial.begin(9600);
-  input_password.reserve(32);
+
   //door lock END
 }
 
 void loop() {
-    //door lock START
+
+
+
+
+  a = Serial2.readString();
+  Serial.println(a);
+  
   //door lock START
-if(feature==1){
+  //door lock START
+  if (feature == 1) {
 
 
-  char key = keypad.getKey();
+    if (a == o) {
+      
 
-  if (key) {
-    Serial.println(key);
-
-    if (key == '*') {
-      input_password = "";  // clear input password
-    } else if (key == '#') {
-      if (open_password == input_password) {
-        Serial.println("The password is correct, ACCESS GRANTED!");
-        // DO YOUR WORK HERE
-        for (pos = 0; pos <= 90; pos += 1) {  // goes from 0 degrees to 180 degrees
-          // in steps of 1 degree
-          myservo.write(pos);  // tell servo to go to position in variable 'pos'
-          delay(5);            // waits 15ms for the servo to reach the position
-        }
+      for (pos = 0; pos <= 90; pos += 1) {  // goes from 0 degrees to 180 degrees
+        // in steps of 1 degree
+        myservo.write(pos);  // tell servo to go to position in variable 'pos'
+        delay(5);
+         // waits 15ms for the servo to reach the position
+       
       }
-      if (close_password == input_password) {
-        for (pos = 90; pos >= 0; pos -= 1) {  // goes from 180 degrees to 0 degrees
-          myservo.write(pos);                 // tell servo to go to position in variable 'pos'
-          delay(5);                           // waits 15ms for the servo to reach the position
-        }
-        feature = 2;
+    }
+    if (a == c) {
+      for (pos = 90; pos >= 0; pos -= 1) {  // goes from 180 degrees to 0 degrees
+        myservo.write(pos);                 // tell servo to go to position in variable 'pos'
+        delay(5);
+         // waits 15ms for the servo to reach the position
       }
-
-
-      else {
-        Serial.println("The password is incorrect, ACCESS DENIED!");
-      }
-
-      input_password = "";  // clear input password
-    } else {
-      input_password += key;  // append new character to input password string
     }
   }
-  
-}
   //door lock END
   //door lock END
 
@@ -104,28 +87,45 @@ if(feature==1){
 
   // Garage START
   // Garage START
-  if(feature==2){
+  if (feature == 2) {
 
-  // Rotate CW slowly at 5 RPM
-  myStepper.setSpeed(15);
-  myStepper.step(stepsPerRevolution);
-  delay(1000);
+    // Rotate CW slowly at 5 RPM
+    garageStepper.setSpeed(15);
+    garageStepper.step(stepsPerRevolution);
+    delay(1000);
 
-  // Rotate CCW quickly at 10 RPM
-  myStepper.setSpeed(15);
-  myStepper.step(-stepsPerRevolution);
-  delay(1000);
+    // Rotate CCW quickly at 10 RPM
+    garageStepper.setSpeed(15);
+    garageStepper.step(-stepsPerRevolution);
+    delay(1000);
 
-  // feature = 1;
+    feature = 3;
   }
 
   //Garage END
   //Garage END
 
+  //Window START 
+  //Window START
+  if (feature == 3) {
+
+    // Rotate CW slowly at 5 RPM
+    windowStepper.setSpeed(15);
+    windowStepper.step(stepsPerRevolution);
+    delay(1000);
+
+    // Rotate CCW quickly at 10 RPM
+    windowStepper.setSpeed(15);
+    windowStepper.step(-stepsPerRevolution);
+    delay(1000);
+
+    feature = 2;
+  }
 
 
+  //Window END
+  //Window END
 }
 
 //ESP32 Pin 13 for servo input door lock
-//ESP32 Pin 12, 14, 27, 26, 25, 33, 32, 15 for 4x4
 //ESP32 Pin 21, 19, 18, 5
